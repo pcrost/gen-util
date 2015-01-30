7c6279cb05575dbacbc7762c7265af607f6a5045
Peter Crosthwaite <crosthwaite.peter@gmail.com>
2015-01-29 22:00:34 -0800

    dly: Add flow control
    Add optional ready-valid handshakes on both ends of the delay element.
    
    Signed-off-by: Peter Crosthwaite <crosthwaite.peter@gmail.com>

--- dly.vhd
+++ dly.vhd
@@ -29,11 +29,15 @@ entity delay is
 	generic (
 		N : integer := 5;
 		T : integer := 2;
-		DEFAULT : std_logic := '0'
+		DEFAULT : std_logic := 'X'
 	);
 	port (
 		ipt : in std_logic_vector(N-1 downto 0);
 		opt : out std_logic_vector(N-1 downto 0);
+		vld_i : in std_logic := '1';
+		rdy_i : out std_logic;
+		vld_o : out std_logic;
+		rdy_o : in std_logic := '1';
 		clk : in std_logic;
 		rst : in std_logic
 		--clk_rst_GCR : GCR
@@ -48,15 +52,27 @@ architecture Behavioral of delay is
 	type PIPE_TYPE is array (T downto 0) of std_logic_vector(N-1 downto 0);
 	signal pipe : PIPE_TYPE;
 
+	signal vld_stages, rdy_stages : std_logic_vector(T downto 0);
+
 begin
 
-	pipe(0) <= ipt;
+	pipe(0) <= ipt when vld_i = '1' else (others => DEFAULT);
+	vld_stages(0) <= vld_i;
+	rdy_i <= rdy_stages(0);
+
+	vld_o <= vld_stages(T);
+	rdy_stages(T) <= rdy_o;
 	opt <= pipe(T);
 
 	generate_pipe : for I in 1 to T generate
 
+		rdy_stages(I-1) <= not vld_stages(I) or rdy_stages(I);
+
 		process (clk) begin if (clk'event and clk = '1') then
-			pipe(I) <= pipe(I-1);
+			if (rdy_stages(I-1)) then
+				pipe(I) <= pipe(I-1);
+				vld_stages(I) <= vld_stages(I-1);
+			end if;
 
 			if (rst = '1') then
 				pipe(I) <= (others => DEFAULT);
