d5e0774d35019aa7b4341ab462ce7baff5e6816f
Peter Crosthwaite <crosthwaite.peter@gmail.com>
2016-03-26 13:24:37 -0700

    tools: vhdx-filter: open and check files before forking
    Do the target open command first. If it breaks short return. This
    avoids reaching the pipe logic with a bad open command.
    
    Signed-off-by: Peter Crosthwaite <crosthwaite.peter@gmail.com>

--- tools/vhdx-filter.c
+++ tools/vhdx-filter.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdint.h>
+#include <errno.h>
 
 typedef int (*open_f_type)(const char *pathname, int flags, ...);
 
@@ -29,12 +30,17 @@ extern int __O_CREAT__;
 int open(const char *pathname, int flags, mode_t mode)
 {
 
+    int fd = DO_OPEN(pathname, flags, mode);
+    int open_errno = errno;
+
     fprintf(stderr, "intercepting open of: %s\n", pathname);
 
-    if (memcmp(&pathname[strlen(pathname) - 5], ".vhdx", 5) &&
-        memcmp(&pathname[strlen(pathname) - 6], ".vhd.c", 6)) {
+    if ((memcmp(&pathname[strlen(pathname) - 5], ".vhdx", 5) &&
+         memcmp(&pathname[strlen(pathname) - 6], ".vhd.c", 6)) ||
+         fd == -1) {
         fprintf(stderr, "normal open: %s\n", pathname);
-        return DO_OPEN(pathname, flags, mode);
+        errno = open_errno;
+        return fd;
     }
 
     fprintf(stderr, "%s'ing: %s\n", COMMAND, pathname);
@@ -47,7 +53,7 @@ int open(const char *pathname, int flags, mode_t mode)
         close(outpipefd[0]);
         dup2(outpipefd[1], STDOUT_FILENO);
 
-        dup2(DO_OPEN(pathname, flags, mode), STDIN_FILENO);
+        dup2(fd, STDIN_FILENO);
 
         execlp(COMMAND, COMMAND, NULL);
         perror(COMMAND);
