0c8a7cc1fc5a36553f07d7bef5a5d3e73a606cf9
Peter Crosthwaite <crosthwaite.peter@gmail.com>
2016-04-10 04:04:40 -0700

    util: add log2 and count_ones functions
    Signed-off-by: Peter Crosthwaite <crosthwaite.peter@gmail.com>

--- util.v
+++ util.v
@@ -3,6 +3,29 @@ package util;
     typedef struct packed {
         bit clk;
         bit rst;
-    };
+    } Gcr;
+
+    function integer log2(integer n);
+        integer v;
+        integer ret;
+        v = n;
+        ret = 0;
+        while (v > 1) begin
+            ret = ret + 1;
+            v = v / 2;
+        end
+        return ret;
+    endfunction
+
+    function count_ones(integer a, integer len);
+        integer ret;
+        ret = 0;
+        for (integer i = 0; i < len; i++) begin
+            if (a & (1 << i)) begin
+                ret = ret + 1;
+            end
+        end
+        return ret;
+    endfunction
 
 endpackage
